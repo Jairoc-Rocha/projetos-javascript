cd3eb54493e094eaeebe49974e843adc5435abf8
Jairo Rocha <jairoc.rocha10@gmail.com>
2023-08-23 15:20:46 -0300

    Finalizando o projeto 4_galera_com_lightbox

---
+++ 4_galera_com_lightbox/scripts.js
@@ -0,0 +1,18 @@
+const galleryItem = document.querySelectorAll(".gallery-item");
+const lightbox = document.querySelector(".lightbox");
+const lightboxImage = document.querySelector(".lightbox-image");
+const lightboxClose = document.querySelector(".lightbox-close");
+
+galleryItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    const imageUrl = item
+      .querySelector(".gallery-image")
+      .getAttribute("data-src");
+    lightboxImage.setAttribute("src", imageUrl);
+    lightbox.style.display = "flex";
+  });
+});
+
+lightboxClose.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
